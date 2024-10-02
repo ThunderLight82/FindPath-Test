0ce19524413796bf8454d1a2ccbdc6ccf9224f3e
Pecherky Oleg <yogidanfuzo@gmail.com>
2024-10-02 16:10:40 +0300

    Small improvements to tests readability
    Organizes a unit tests into three "AAA" parts

--- FindPath.Tests/SampleTests.cs
+++ FindPath.Tests/SampleTests.cs
@@ -2,103 +2,111 @@ using FindPath.App;
 using NUnit.Framework;
 using System.Drawing;
 
-namespace FindPath.Tests
+namespace FindPath.Tests;
+
+public class Tests
 {
-    public class Tests
+    [Test]
+    public void FindShortestPath_EmptyArray_ReturnsDirectPath()
     {
-        [Test]
-        public void FindShortestPath_EmptyArray_ReturnsDirectPath()
+        //Arrange
+        int[,] arr = new int[4, 4]
         {
-            int[,] arr = new int[4, 4]
-            {
-                {0,0,0,0},
-                {0,0,0,0},
-                {0,0,0,0},
-                {0,0,0,0}
-            };
+            {0,0,0,0},
+            {0,0,0,0},
+            {0,0,0,0},
+            {0,0,0,0}
+        };
 
-            var expectedResult = new[]
-            {
-                new Point(0,0),
-                new Point(0,1),
-                new Point(1,1),
-                new Point(1,2),
-                new Point(2,2),
-                new Point(2,3),
-                new Point(3,3)
-            };
+        var expectedResult = new[]
+        {
+            new Point(0,0),
+            new Point(0,1),
+            new Point(1,1),
+            new Point(1,2),
+            new Point(2,2),
+            new Point(2,3),
+            new Point(3,3)
+        };
 
-            var actualResult = Program.FindShortestPath(arr);
+        //Act
+        var actualResult = Program.FindShortestPath(arr);
 
-            Assert.IsNotNull(actualResult);
-            Assert.That(actualResult.Length, Is.EqualTo(expectedResult.Length));
+        //Assert
+        Assert.IsNotNull(actualResult);
+        Assert.That(actualResult.Length, Is.EqualTo(expectedResult.Length));
 
-            for(int i = 0; i < expectedResult.Length; i++)
-            {
-                Assert.That(actualResult[i], Is.EqualTo(expectedResult[i]));
-            }
+        for (int i = 0; i < expectedResult.Length; i++)
+        {
+            Assert.That(actualResult[i], Is.EqualTo(expectedResult[i]));
         }
+    }
 
-        [Test]
-        public void FindShortestPath_UniformArray_ReturnsDirectPath()
+    [Test]
+    public void FindShortestPath_UniformArray_ReturnsDirectPath()
+    {
+        //Arrange
+        int[,] arr = new int[4, 4]
         {
-            int[,] arr = new int[4, 4]
-            {
-                {1,1,1,1},
-                {1,1,1,1},
-                {1,1,1,1},
-                {1,1,1,1}
-            };
+            {1,1,1,1},
+            {1,1,1,1},
+            {1,1,1,1},
+            {1,1,1,1}
+        };
 
-            var expectedResult = new[]
-            {
-                new Point(0,0),
-                new Point(1,1),
-                new Point(2,2),
-                new Point(3,3)
-            };
+        var expectedResult = new[]
+        {
+            new Point(0,0),
+            new Point(1,1),
+            new Point(2,2),
+            new Point(3,3)
+        };
 
-            var actualResult = Program.FindShortestPath(arr);
+        //Act
+        var actualResult = Program.FindShortestPath(arr);
 
-            Assert.IsNotNull(actualResult);
-            Assert.That(actualResult.Length, Is.EqualTo(expectedResult.Length));
+        //Assert
+        Assert.IsNotNull(actualResult);
+        Assert.That(actualResult.Length, Is.EqualTo(expectedResult.Length));
 
-            for (int i = 0; i < expectedResult.Length; i++)
-            {
-                Assert.That(actualResult[i], Is.EqualTo(expectedResult[i]));
-            }
+        for (int i = 0; i < expectedResult.Length; i++)
+        {
+            Assert.That(actualResult[i], Is.EqualTo(expectedResult[i]));
         }
+    }
 
-        [Test]
-        public void FindShortestPath_MixedArray_ReturnsShortestPath()
+    [Test]
+    public void FindShortestPath_MixedArray_ReturnsShortestPath()
+    {
+        //Arrange
+        int[,] arr = new int[4, 4]
         {
-            int[,] arr = new int[4, 4]
-            {
-                {1,1,0,1},
-                {1,1,0,1},
-                {0,0,4,1},
-                {1,1,1,1}
-            };
+            {1,1,0,1},
+            {1,1,0,1},
+            {0,0,4,1},
+            {1,1,1,1}
+        };
 
-            var expectedResult = new[]
-            {
-                new Point(0,0),
-                new Point(1,1),
-                new Point(1,2),
-                new Point(1,3),
-                new Point(2,3),
-                new Point(3,3)
-            };
+        var expectedResult = new[]
+        {
+            new Point(0,0),
+            new Point(1,1),
+            new Point(1,2),
+            new Point(1,3),
+            new Point(2,3),
+            new Point(3,3)
+        };
 
-            var actualResult = Program.FindShortestPath(arr);
+        //Act
+        var actualResult = Program.FindShortestPath(arr);
 
-            Assert.IsNotNull(actualResult);
-            Assert.That(actualResult.Length, Is.EqualTo(expectedResult.Length));
+        //Assert
+        Assert.IsNotNull(actualResult);
+        Assert.That(actualResult.Length, Is.EqualTo(expectedResult.Length));
 
-            for (int i = 0; i < expectedResult.Length; i++)
-            {
-                Assert.That(actualResult[i], Is.EqualTo(expectedResult[i]));
-            }
+        for (int i = 0; i < expectedResult.Length; i++)
+        {
+            Assert.That(actualResult[i], Is.EqualTo(expectedResult[i]));
         }
     }
 }
